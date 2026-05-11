7524f58cdbb1f386392e4450efe38534f6f0f913
youngman <sksn12@naver.com>
2026-05-11 12:07:47 +0900

    ts

---
+++ src/test.java
@@ -0,0 +1,15 @@
+public interface test {
+
+    int s = 10;
+
+    void print();
+
+    void print2();
+}
+
+
+class Test {
+    public static void main(String[] args) {
+        System.out.println(test.s);
+    }
+}
