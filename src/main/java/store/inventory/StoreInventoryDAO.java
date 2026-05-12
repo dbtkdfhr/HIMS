617b124988f3897832d54ebf32100b0fefbb5d12
dbtkdfhr <dbtkdfhr2000@kookmin.ac.kr>
2026-05-12 15:29:35 +0900

    feat: 매장 재고 수량 증가 함수 추가

--- src/main/java/store/inventory/StoreInventoryDAO.java
+++ src/main/java/store/inventory/StoreInventoryDAO.java
@@ -33,4 +33,29 @@ public class StoreInventoryDAO {
       DBConnection.close(pstmt);
     }
   }
+
+  public int increaseQuantity(Connection conn, long storeId, long productId, int quantity)
+      throws SQLException {
+    String increaseQuantitySql = "";
+    increaseQuantitySql += "UPDATE STORE_INVENTORY ";
+    increaseQuantitySql += "SET current_quantity = current_quantity + ?, ";
+    increaseQuantitySql += "updated_at = SYSDATE ";
+    increaseQuantitySql += "WHERE store_id = ? ";
+    increaseQuantitySql += "AND product_id = ? ";
+    increaseQuantitySql += "AND ? > 0";
+
+    PreparedStatement pstmt = null;
+
+    try {
+      pstmt = conn.prepareStatement(increaseQuantitySql);
+      pstmt.setInt(1, quantity);
+      pstmt.setLong(2, storeId);
+      pstmt.setLong(3, productId);
+      pstmt.setInt(4, quantity);
+
+      return pstmt.executeUpdate();
+    } finally {
+      DBConnection.close(pstmt);
+    }
+  }
 }
