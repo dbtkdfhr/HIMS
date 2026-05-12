f18edddde90c542bba20a8ea8a251c803f3640e9
dbtkdfhr <55170493+dbtkdfhr@users.noreply.github.com>
2026-05-12 11:08:04 +0900

    feat(#10): 입고 검수 대상을 위한 DTO, DAO 구현 (#11)
    * feat: 발주 요청 DTO 구현
    
    * feat: 발주 요청 DAO 구현, order status에 따른 select 함수 구현
    
    * feat: 타입 수정

---
+++ src/main/java/order/request/OrderRequestDAO.java
@@ -0,0 +1,93 @@
+package order.request;
+
+import common.DBConnection;
+import common.DBType;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderRequestDAO {
+
+  public List<OrderRequestDTO> findByOrderRequestIdAndOrderStatus(long orderRequestId, String orderStatus)
+      throws SQLException {
+    List<OrderRequestDTO> orderRequestDTOList = new ArrayList<>();
+
+    String sql = "SELECT * FROM ORDER_REQUEST WHERE order_request_id = ? AND order_status = ?";
+
+    Connection connection = null;
+    PreparedStatement preparedStatement = null;
+    ResultSet resultSet = null;
+
+    try {
+      connection = DBConnection.getConnection(DBType.ORACLE);
+      preparedStatement = connection.prepareStatement(sql);
+      preparedStatement.setLong(1, orderRequestId);
+      preparedStatement.setString(2, orderStatus);
+
+      resultSet = preparedStatement.executeQuery();
+
+      while (resultSet.next()) {
+        orderRequestDTOList.add(mapToOrderRequestDTO(resultSet));
+      }
+    } catch (SQLException e) {
+      throw e;
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    } finally {
+      DBConnection.close(resultSet);
+      DBConnection.close(preparedStatement);
+      DBConnection.close(connection);
+    }
+
+    return orderRequestDTOList;
+  }
+
+  private OrderRequestDTO mapToOrderRequestDTO(ResultSet resultSet) throws SQLException {
+    OrderRequestDTO dto = new OrderRequestDTO();
+
+    dto.setOrderRequestId(resultSet.getLong("ORDER_REQUEST_ID"));
+    dto.setStoreId(resultSet.getLong("STORE_ID"));
+    dto.setSupplierIntegrationId(resultSet.getLong("SUPPLIER_INTEGRATION_ID"));
+    dto.setProductId(resultSet.getLong("PRODUCT_ID"));
+    dto.setRequestEmployeeId(resultSet.getLong("REQUEST_EMPLOYEE_ID"));
+    dto.setApprovalEmployeeId(getNullableLong(resultSet, "APPROVAL_EMPLOYEE_ID"));
+    dto.setApprovalRoleId(getNullableLong(resultSet, "APPROVAL_ROLE_ID"));
+    dto.setExternalOrderId(resultSet.getString("EXTERNAL_ORDER_ID"));
+    dto.setOrderQuantity(resultSet.getInt("ORDER_QUANTITY"));
+    dto.setApprovedQuantity(getNullableLong(resultSet, "APPROVED_QUANTITY"));
+    dto.setRequestReason(resultSet.getString("REQUEST_REASON"));
+    dto.setRejectReason(resultSet.getString("REJECT_REASON"));
+    dto.setOrderStatus(resultSet.getString("ORDER_STATUS"));
+    dto.setRequestedAt(getNullableLocalDateTime(resultSet, "REQUESTED_AT"));
+    dto.setApprovedAt(getNullableLocalDateTime(resultSet, "APPROVED_AT"));
+    dto.setRejectedAt(getNullableLocalDateTime(resultSet, "REJECTED_AT"));
+    dto.setSentToSupplierAt(getNullableLocalDateTime(resultSet, "SENT_TO_SUPPLIER_AT"));
+
+    return dto;
+  }
+
+  private Long getNullableLong(ResultSet resultSet, String columnName) throws SQLException {
+    long value = resultSet.getLong(columnName);
+
+    if (resultSet.wasNull()) {
+      return null;
+    }
+
+    return value;
+  }
+
+  private LocalDateTime getNullableLocalDateTime(ResultSet resultSet, String columnName) throws SQLException {
+    Timestamp value = resultSet.getTimestamp(columnName);
+
+    if (value == null) {
+      return null;
+    }
+
+    return value.toLocalDateTime();
+  }
+}
