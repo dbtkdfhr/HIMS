0f7cb9d2f5bc5d0e00d20bb298acae9671503dbc
dbtkdfhr <dbtkdfhr2000@kookmin.ac.kr>
2026-05-12 10:46:28 +0900

    feat: 발주 요청 DTO 구현

---
+++ src/main/java/order/request/OrderRequestDTO.java
@@ -0,0 +1,26 @@
+package order.request;
+
+import java.time.LocalDateTime;
+import lombok.Data;
+
+@Data
+public class OrderRequestDTO {
+
+  private long orderRequestId;
+  private long storeId;
+  private long supplierIntegrationId;
+  private long productId;
+  private long requestEmployeeId;
+  private Long approvalEmployeeId; // long과 Long은 not null, nullable 차이를 위해 분리
+  private Long approvalRoleId;
+  private String externalOrderId;
+  private int orderQuantity;
+  private Long approvedQuantity;
+  private String requestReason;
+  private String rejectReason;
+  private String orderStatus; // REQUESTED, APPROVED, REJECTED, SENT, RECEIVED, CANCELED
+  private LocalDateTime requestedAt;
+  private LocalDateTime approvedAt;
+  private LocalDateTime rejectedAt;
+  private LocalDateTime sentToSupplierAt;
+}
