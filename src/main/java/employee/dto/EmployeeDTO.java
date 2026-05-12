7d0d797d560762725af66ed2a3b56c77874a55f7
WOOJIN CHOI <dnwls0723@sookmyung.ac.kr>
2026-05-12 10:54:32 +0900

    fix: DTO 필드 정리 및 Lombok 어노테이션 분리

--- src/main/java/employee/dto/EmployeeDTO.java
+++ src/main/java/employee/dto/EmployeeDTO.java
@@ -2,19 +2,30 @@ package employee.dto;
 
 import java.time.LocalDateTime;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(exclude = "password")
 public class EmployeeDTO {
 
   private Long employeeId;
-  private Long roleId;
-  private Long storeId;
   private String loginId;
   private String password;
   private String employeeName;
   private String phoneNumber;
+
+  private Long roleId;
+  private Long storeId;
+
   private String isActive;
+
   private LocalDateTime createdAt;
   private LocalDateTime updatedAt;
 }
