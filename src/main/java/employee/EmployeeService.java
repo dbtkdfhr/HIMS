35b0d6d7eb7d684ef026a71a05df7b761b70703c
WOOJIN CHOI <dnwls0723@sookmyung.ac.kr>
2026-05-13 18:11:44 +0900

    feat: 클래스 생성자 추가

--- src/main/java/employee/EmployeeService.java
+++ src/main/java/employee/EmployeeService.java
@@ -6,7 +6,11 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class EmployeeService {
-  private final EmployeeDAO employeeDAO = new EmployeeDAO();
+  private final EmployeeDAO employeeDAO;
+
+  public EmployeeService(EmployeeDAO employeeDAO) {
+    this.employeeDAO = employeeDAO;
+  }
 
   public List<String> findAllEmployeeSummaries() throws SQLException {
     return employeeDAO.findAllEmployeeSummaries();
