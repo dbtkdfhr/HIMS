a8d26bfdfcf30e6cec670c5bf59d858633306a7c
dbtkdfhr <dbtkdfhr2000@kookmin.ac.kr>
2026-05-13 09:42:17 +0900

    feat: updated at, receipt status와 external order receipt 값 연동

--- db/procedure/procRestockSupplierInventory.sql
+++ db/procedure/procRestockSupplierInventory.sql
@@ -4,8 +4,7 @@ CREATE PROCEDURE procRestockSupplierInventory()
 BEGIN
     -- 현재 재고(CURRENT_QUANTITY)가 999개 이하인 품목에 대해서만 1,000개를 추가 생산(UPDATE)
     UPDATE supplier_inventory
-    SET current_quantity = current_quantity +1000,
-        updated_at = now()
+    SET current_quantity = current_quantity +1000
     WHERE current_quantity <=999;
 END //
 
